5aa8628d239e91e1eaf4364c9e6949263dd29c4d
unknown <marcos_echazarret@hotmail.com>
2015-09-24 17:14:35 -0500

    main.js

--- goal-based/main.js
+++ goal-based/main.js
@@ -4,7 +4,7 @@ var Strategy = require("./breath");
 
 
 var strategy = new Strategy();
-var problem = new Problem({espacio1:2,espacio2:3,espacio3:4,espacio4:5,espacio5:6,espacio6:7,espacio7:8,espacio8:9,espacio9:0});
+var problem = new Problem({none1:2,none2:3,none3:4,none4:5,none5:6,none6:7,none7:8,none8:9,none9:0});
 
 var search = new Search(problem,strategy);
 
