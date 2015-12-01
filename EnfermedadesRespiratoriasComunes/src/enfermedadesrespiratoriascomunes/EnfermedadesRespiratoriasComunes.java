1e9c225d20a08383e1b1d0eeedb3dbb0dc4119eb
SilviaSanchez <chivis_mejor@hotmail.com>
2015-11-30 21:52:20 -0600

    proyecto

---
+++ EnfermedadesRespiratoriasComunes/src/enfermedadesrespiratoriascomunes/EnfermedadesRespiratoriasComunes.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package enfermedadesrespiratoriascomunes;
+
+
+
+
+
+/**
+ *
+ * @author Aivlis
+ */
+public class EnfermedadesRespiratoriasComunes {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+        
+           // TODO code application logic here
+    }
+    
+}
